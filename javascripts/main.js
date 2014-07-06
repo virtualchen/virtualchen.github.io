ab0214a240632c9cde94ad4bb341046c6dbefc3d
virtualchen <virtualchen@gmail.com>
2014-07-06 16:08:06 +0800

    close #2

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,7 +4,36 @@
         //url: 'http://www.mokoversity.com/1/post/tags/fullstack',
         url: 'http://booklog.io/1/post',
         success: function (data){
-           $('#postTemplate').tmpl(data.posts).appendTo('#content');
-        }
+        	$('#postTemplate').tmpl(data.posts).appendTo('#content');
+
+        	/*$('[id=subject]').each(function(this){
+        		var obj = $(this);
+
+        	});*/
+        	var subjects = $('[id=subject]');
+    
+
+  			$.each( subjects, function(index,obj){
+  				console.log(obj);
+  				var id = $(obj).data("id");
+  				$(obj).click(function(){
+  						var targeturl ='http://booklog.io/1/post/' + id; 
+						console.log(targeturl);
+						$.ajax({
+							dataType: 'json',
+							url: targeturl,
+							success: function(data){
+								//console.log(targeturl);
+								var html = '<h1>';
+								html += data.post.content;
+								html += '</h1>'; 
+								var contents= $('.innerContent');
+								contents[index].innerHTML = html;  
+							}
+						})  ;	
+  				});
+
+			});
+		}
     });
 })($);
