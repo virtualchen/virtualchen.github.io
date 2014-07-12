7987e296fd18cc97776142bee0a5fdcbec3c1f4f
virtualchen <virtualchen@gmail.com>
2014-07-12 10:58:40 +0800

    correct url

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,6 +3,7 @@
         dataType: 'json',
         //url: 'http://www.mokoversity.com/1/post/tags/fullstack',
         url: 'http://booklog.io/1/post',
+     
         success: function (data){
         	$('#postTemplate').tmpl(data.posts).appendTo('#content');
 
